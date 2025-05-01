80962165e9a8ecf0102f4afd888dbb69a794ada6
juninhopo <kelgaxd@gmail.com>
2025-05-01 19:42:00 -0400

    feat: adjust the validation file type

--- src/utils/upload.ts
+++ src/utils/upload.ts
@@ -1,41 +1,29 @@
-/**
- * File upload middleware configuration
- */
-
 import multer from 'multer';
 import path from 'path';
 import fs from "fs"
 import { fileTypeFromBuffer } from 'file-type';
 import { cleanupFiles } from './imageUtils';
 
-// Setup file uploads
 export const upload = multer({
-  dest: 'uploads/',
+  storage: multer.memoryStorage(), // Usar armazenamento em memória em vez de disco
   limits: {
     fileSize: 5 * 1024 * 1024, // 5MB max file size
   },
   fileFilter: async (_req, file, cb) => {
     try {
-    const filePath = file.path;
+      const validMimeTypes = ['image/jpeg', 'image/png', 'image/jpg'];
+      const validExtensions = ['.jpg', '.jpeg', '.png'] 
+      
+      const fileExtension = path.extname(file.originalname).toLowerCase();
+      
+      if (!validExtensions.includes(fileExtension) || !validMimeTypes.includes(file.mimetype)) {
+        console.log('Invalid image format. Only JPG, JPEG, and PNG are supported.')
+        return cb(new Error('Invalid image format. Only JPG, JPEG, and PNG are supported.'));
+      }
+      
+      return cb(null, true);
 
-    const buffer = fs.readFileSync(filePath);
-
-    const validMimeTypes = ['image/jpeg', 'image/png', 'image/jpg'];
-    const validExtensions = ['.jpg', '.jpeg', '.png'] 
-    
-    const fileExtension = path.extname(file.originalname).toLowerCase();
-    
-    const type = await fileTypeFromBuffer(buffer);
-
-    if (!validExtensions.includes(fileExtension) || !type || !validMimeTypes.includes(type.mime)) {
-       cleanupFiles([file.path]);
-
-      return cb(new Error('Invalid image format. Only JPG, JPEG, and PNG are supported.'));
-    }
-    
-    return cb(null, true);
-
-  } catch (error) {
+    } catch (error) {
       console.error(error)
 
       return cb(new Error('Error validating file'));
